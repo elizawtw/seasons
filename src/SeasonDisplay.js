9dbbfc28043c334b1086df709e15d73377f8c864
eliza <twew02@gmail.com>
2022-11-08 11:42:59 -0500

    work on seasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,24 @@
 import React from "react";
+//determining season
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
 
 const SeasonDisplay = (props) => {
-  console.log(props.lat)
+  
+const season = getSeason(props.lat, new Date().getMonth());
+const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+const icon = season === 'winter' ? 'snowflake' : 'sun';
   return (
-    <div>Hi there</div>
+    <div>
+    <i className={`${icon} icon`} />
+    <h1>{text}</h1>
+    <i className={`${icon} icon`} />
+    </div>
   )
 }
 
