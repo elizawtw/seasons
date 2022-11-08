9a672d3c94f5f577d0042fa9092b43a166001b41
eliza <twew02@gmail.com>
2022-11-08 11:06:21 -0500

    initial setup

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SeasonDisplay = (props) => {
+  console.log(props.lat)
+  return (
+    <div>Hi there</div>
+  )
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  //not required by react but required by javascript whenever class is created. This is where we can initialize state
+  // constructor(props) {
+  //   super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //   this.state = { lat: null, long: null, errorMsg: "" }; 
+  // }
+
+  state = { lat: null, long: null, errorMsg: "" };//where we initialize state object, THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO STATE
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+       
+      (position) => {this.setState({
+        lat: position.coords.latitude, 
+        long: position.coords.longitude
+      })},
+      //handle error using state
+      (err) => {this.setState({ 
+        errorMsg: err.message
+       })}
+
+    );
+  }
+
+  //React says we have to define render()
+  render() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+    if (this.state.lat && !this.state.errorMsg) {
+      return (
+        
+          <SeasonDisplay lat={this.state.lat} />
+      );
+    }
+    return <h2>Loading...</h2>;
+  }
+}
+
+const root = ReactDOM.render(<App />, document.querySelector("#root"));
